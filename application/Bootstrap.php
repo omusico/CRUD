4405e04719c62aa8971fc257cea9c6bcac52752c
elvis <elvis@90cfc9fa-5053-4b20-8720-174ebf44e258>
2010-11-27 10:03:52 +0000

    added autoloader for forms and order forms into the Boostrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,10 +2,31 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    /**
+     * Add autoloader for instances
+     * @return <type>
+     */
     public function _initLoader()
     {
-        //pd(Zend_Loader_Autoloader::getInstance());
+        $crudAutoloader = new Zend_Application_Module_Autoloader(
+            array(
+                'namespace' => 'Application_',
+                'basePath'  => dirname(__FILE__),
+            )
+        );
+        $crudAutoloader->addResourceTypes(
+            array(
+                'form_order'    => array(
+                    'namespace' => 'Form_Order',
+                    'path'      => 'forms/order',
+                ),
+                'form_filter'    => array(
+                    'namespace' => 'Form_Filter',
+                    'path'      => 'forms/filter',
+                ),
+            )
+        );
+        return $crudAutoloader;
     }
-
 }
 
